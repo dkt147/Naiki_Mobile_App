b913ecdd18ea2503082301e921381fe97072ed57
mr-mubeen <mubeen13132@gmail.com>
2021-07-02 03:41:16 +0500

    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master) $ git init Reinitialized existing Git repository in L:/Users/Mubeen/AndroidStudioProjects/Naiki/.git/
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git status
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            .idea/vcs.xml
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git add -a
    error: unknown switch `a'
    usage: git add [<options>] [--] <pathspec>...
    
        -n, --dry-run         dry run
        -v, --verbose         be verbose
    
        -i, --interactive     interactive picking
        -p, --patch           select hunks interactively
        -e, --edit            edit current diff and apply
        -f, --force           allow adding otherwise ignored files
        -u, --update          update tracked files
        --renormalize         renormalize EOL of tracked files (implies -u)
        -N, --intent-to-add   record only the fact that the path will be added later
        -A, --all             add changes from all tracked and untracked files
        --ignore-removal      ignore paths removed in the working tree (same as --no-all)
        --refresh             don't add, only refresh the index
        --ignore-errors       just skip files which cannot be added because of errors
        --ignore-missing      check if - even missing - files are ignored in dry run
        --chmod (+|-)x        override the executable bit of the listed files
        --pathspec-from-file <file>
                              read pathspec from file
        --pathspec-file-nul   with --pathspec-from-file, pathspec elements are separated with NUL character
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git status
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            .idea/vcs.xml
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ cd Naiki
    bash: cd: Naiki: No such file or directory
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git push origin master
    
    fatal: unable to access 'https://github.com/mr-mubeen/Naiki-DonationApp.git/': Could not resolve host: github.com
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)updated

--- app/src/main/java/com/example/naiki/Register.java
+++ app/src/main/java/com/example/naiki/Register.java
@@ -1,9 +1,17 @@
 package com.example.naiki;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,12 +19,22 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Register extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
-    EditText uname , reg_pass , address, email , reg_phone ;
+    EditText uname , reg_pass , email , reg_phone ;
+    TextView address;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     Spinner spinner;
     ArrayList<String> data = new ArrayList<>();
@@ -25,10 +43,11 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         uname = (EditText) findViewById(R.id.username_reg);
         reg_phone = (EditText) findViewById(R.id.phone_reg);
-        address =(EditText)  findViewById(R.id.address_reg);
+        address =(TextView)  findViewById(R.id.address_reg);
         email =(EditText)  findViewById(R.id.email_reg);
         reg_pass =(EditText)  findViewById(R.id.password_reg);
         spinner =  findViewById(R.id.type_reg);
@@ -38,6 +57,9 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+
+
     }
 
 
@@ -56,7 +78,11 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
 
     public void reg(View view){
 
-
+        if (ActivityCompat.checkSelfPermission(Register.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLocation();
+        } else {
+            ActivityCompat.requestPermissions(Register.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
 
         if(uname.length()==0)
         {
@@ -98,5 +124,43 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
             Background_Worker background_worker = new Background_Worker(this);
             background_worker.execute("register", user_text, pass_text , address_text  , phone_text , email_text , tp);
         }
+    }
+
+
+
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null)
+                {
+
+                    try {
+                        Geocoder geocoder = new Geocoder(Register.this , Locale.getDefault());
+                        List<Address> address1 = geocoder.getFromLocation(location.getLatitude() , location.getLongitude() ,1);
+
+                        address.setText(address1.get(0).getAddressLine(0));
+//                        textView2 = findViewById(R.id.textView6);
+//                        textView2.setText(Html.fromHtml("<font> Latitude : </font>"  +address.get(0).getCountryName()));
+//                        textView3 = findViewById(R.id.textView7);
+//                        textView3.setText(Html.fromHtml("<font> Latitude : </font>"  +address.get(0).getLocality()));
+//                        textView4 = findViewById(R.id.textView8);
+//                        textView4.setText(Html.fromHtml("<font> Latitude : </font>"  +address.get(0).getLatitude()));
+//                        textView5 = findViewById(R.id.textView9);
+//                        textView5.setText(Html.fromHtml("<font> Latitude : </font>"  +address.get(0).getLongitude()));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
     }
 }
