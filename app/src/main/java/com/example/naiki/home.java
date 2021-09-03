ff5db88b0c358ae995bc896768291c4fcebd9fd8
mr-mubeen <mubeen13132@gmail.com>
2021-09-03 21:49:15 +0500

    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master) $ git init Reinitialized existing Git repository in L:/Users/Mubeen/AndroidStudioProjects/Naiki/.git/
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git status
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            .idea/vcs.xml
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git add -a
    error: unknown switch `a'
    usage: git add [<options>] [--] <pathspec>...
    
        -n, --dry-run         dry run
        -v, --verbose         be verbose
    
        -i, --interactive     interactive picking
        -p, --patch           select hunks interactively
        -e, --edit            edit current diff and apply
        -f, --force           allow adding otherwise ignored files
        -u, --update          update tracked files
        --renormalize         renormalize EOL of tracked files (implies -u)
        -N, --intent-to-add   record only the fact that the path will be added later
        -A, --all             add changes from all tracked and untracked files
        --ignore-removal      ignore paths removed in the working tree (same as --no-all)
        --refresh             don't add, only refresh the index
        --ignore-errors       just skip files which cannot be added because of errors
        --ignore-missing      check if - even missing - files are ignored in dry run
        --chmod (+|-)x        override the executable bit of the listed files
        --pathspec-from-file <file>
                              read pathspec from file
        --pathspec-file-nul   with --pathspec-from-file, pathspec elements are separated with NUL character
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git status
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            .idea/vcs.xml
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ cd Naiki
    bash: cd: Naiki: No such file or directory
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $ git push origin master
    
    fatal: unable to access 'https://github.com/mr-mubeen/Naiki-DonationApp.git/': Could not resolve host: github.com
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)
    $
    
    Mubeen@DESKTOP-2VSO8HL MINGW64 /l/Users/Mubeen/AndroidStudioProjects/Naiki (master)updated
    git init

--- app/src/main/java/com/example/naiki/home.java
+++ app/src/main/java/com/example/naiki/home.java
@@ -1,5 +1,8 @@
 package com.example.naiki;
 
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +10,180 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link home#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
 public class home extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ListView listView;
+    AlertDialog alertDialog;
+    SharedPreferences sharedPreferences;
+
+    ArrayList<String> donate_list_data = new ArrayList<>();
 
     public home() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static home newInstance(String param1, String param2) {
-        home fragment = new home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        listView = view.findViewById(R.id.donation_list);
+        donate_list();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+
+    private void donate_list() {
+        class bgWorker extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... strings) {
+
+
+                String type = strings[0];
+//                String location = strings[1];
+
+
+                if (type.equals("list")) {
+
+
+                    try {
+
+
+                        String fetch_url = "http://192.168.56.1/naiki/donate_list.php";
+                        URL url = new URL(fetch_url);
+                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                        httpURLConnection.setRequestMethod("POST");
+                        httpURLConnection.setDoOutput(true);
+                        httpURLConnection.setDoInput(true);
+                        OutputStream outputStream = httpURLConnection.getOutputStream();
+                        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+//                        String post_data = URLEncoder.encode("d_id", "UTF-8") + "=" + URLEncoder.encode(employeeid, "UTF-8");
+//                        bufferedWriter.write(post_data);
+                        bufferedWriter.flush();
+                        bufferedWriter.close();
+                        outputStream.close();
+                        InputStream inputStream = httpURLConnection.getInputStream();
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                        String result = "";
+                        String line = "";
+                        while ((line = bufferedReader.readLine()) != null) {
+                            result += line;
+                        }
+                        bufferedReader.close();
+                        inputStream.close();
+                        httpURLConnection.disconnect();
+                        return result;
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return null;
+            }
+
+
+            @Override
+            protected void onPreExecute() {
+
+//                alertDialog = new AlertDialog.Builder(context).create();
+//                alertDialog.setTitle("Apply Leave");
+//                sharedPreferences = context.getSharedPreferences("Mypref", Context.MODE_PRIVATE);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                if (s != null) {
+
+//                if (s.equals("Registered")) {
+//                    alertDialog.setMessage("Employee Registered Succesfully");
+//                    alertDialog.show();}
+//                else {
+//                    alertDialog.setMessage("Register Failed");
+//                    alertDialog.show();
+//                }
+
+                    if (s.equals("Failed")) {
+                        alertDialog.setMessage("Details not found");
+                        alertDialog.show();
+                    } else {
+                        try {
+                            JSONArray jsonArray = new JSONArray(s);
+                            JSONObject jsonObject = null;
+                            donate_list_data.clear();
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                jsonObject = jsonArray.getJSONObject(i);
+                                String row = //jsonObject.getInt("uid")+" "+
+
+                                        "Item Name" + "\t \t \t \t" +
+                                                jsonObject.getString("item_name") + "\n \n" +
+                                                "Category" + "\t \t \t \t" +
+                                                jsonObject.getString("category") + "\n \n" +
+                                                "Quantity" + "\t \t \t \t" +
+                                                //jsonObject.getString("upassword")+" "+
+                                                jsonObject.getString("quantity") + "\n \n" +
+                                                "Note" + "\t \t \t \t" +
+                                                jsonObject.getString("note") + "\n ---------------------------------------------------------------------- \n";
+
+
+                                donate_list_data.add(row);
+                            }
+
+
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, donate_list_data);
+                            listView.setAdapter(adapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+//                    if (s.equals("inserted")) {
+//                        alertDialog.setMessage("Applied Successfully");
+//                        alertDialog.show();
+//                    } else if (s.equals("Failed")) {
+//                        alertDialog.setMessage("Invalid username or password");
+//                        alertDialog.show();
+//                    }
+
+
+                    }
+                }
+
+
+            }
+
+        }
+        bgWorker bg = new bgWorker();
+        bg.execute("list");
+
+
     }
 }
