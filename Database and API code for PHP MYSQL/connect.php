f0c71e2e2eba88b9864a495a58168d8a04a24854
mr-mubeen <mubeen13132@gmail.com>
2021-10-30 01:45:15 +0500

    Profile work and image upload committed

--- Database and API code for PHP MYSQL/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost' , 'root' , '' , 'naiki_db') or die ('Connect Failed');
-
-
-?>
--- Database and API code for PHP MYSQL/donate.sql
+++ Database and API code for PHP MYSQL/donate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2021 at 05:16 PM
+-- Generation Time: Oct 29, 2021 at 10:43 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -36,43 +36,17 @@ CREATE TABLE `donate` (
   `item_image` varchar(255) NOT NULL,
   `r_id` int(11) NOT NULL,
   `status` varchar(100) NOT NULL DEFAULT 'Pending',
-  `recv_id` int(11) NOT NULL
+  `recv_id` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `donate`
 --
 
-INSERT INTO `donate` (`d_id`, `item_name`, `category`, `quantity`, `note`, `item_image`, `r_id`, `status`, `recv_id`) VALUES
-(6, '$_POST[\"it\"];', '', 2, '$_POST[\"ds\"];', '$_POST[\"im\"];', 2, 'Pending', 0),
-(7, 'shoes', '', 2, '', '', 2, 'Pending', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `register`
---
-
-CREATE TABLE `register` (
-  `r_id` int(100) NOT NULL,
-  `uname` varchar(255) NOT NULL,
-  `upass` varchar(255) NOT NULL,
-  `uphone` int(11) NOT NULL,
-  `uaddress` varchar(255) NOT NULL,
-  `uemail` varchar(255) NOT NULL,
-  `utype` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `register`
---
-
-INSERT INTO `register` (`r_id`, `uname`, `upass`, `uphone`, `uaddress`, `uemail`, `utype`) VALUES
-(1, 'ali', 'ali123', 13123, 'adas', 'adas', 'dsf'),
-(2, 'ali', 'ali123', 3123, 'adas', 'adas', 'dsf'),
-(3, 'asdasd', 'asdasd', 213213, 'asdas', 'sadfasd', ''),
-(4, 'bilal', 'asdas', 1313123, 'sadad', 'sdasd', 'Donor'),
-(5, 'sada', 'aasd', 721, 'Address : 1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA', 'asd', 'Donor');
+INSERT INTO `donate` (`d_id`, `item_name`, `category`, `quantity`, `note`, `item_image`, `r_id`, `status`, `recv_id`, `type`) VALUES
+(5, 'shoes', 'School Item', 2, 'shoes', 'IMG1361196666.jpeg', 7, 'Pending', 0, 'Donate'),
+(6, 'coloths', 'Clothes', 3, 'clothe', 'IMG1748977064.jpeg', 7, 'Pending', 0, 'Donate');
 
 --
 -- Indexes for dumped tables
@@ -86,12 +60,6 @@ ALTER TABLE `donate`
   ADD KEY `r_id` (`r_id`);
 
 --
--- Indexes for table `register`
---
-ALTER TABLE `register`
-  ADD PRIMARY KEY (`r_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -99,13 +67,7 @@ ALTER TABLE `register`
 -- AUTO_INCREMENT for table `donate`
 --
 ALTER TABLE `donate`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `register`
---
-ALTER TABLE `register`
-  MODIFY `r_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
